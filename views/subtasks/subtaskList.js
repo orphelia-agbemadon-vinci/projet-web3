1c233819a9a82476c1f9f781df038484dbb45be1
oceane.tran <thuyduong.tran@student.vinci.be>
2024-11-19 21:22:02 +0100

    fix: remove redundant title from subtask list header

--- views/subtasks/subtaskList.js
+++ views/subtasks/subtaskList.js
@@ -3,7 +3,7 @@ import createASubtask from './subtask.js';
 const createSubtaskList = (subtasks, task) => /*html*/ `
     <div id="subtask-list">
         <div>
-            <h2>Sous-tâches de la tâche : ${task.description}</h2>
+            <h2>${task.description}</h2>
         </div>
         <form hx-post="/tasks/subtasks/add/${task.id}" hx-target="#subtask-list" hx-swap="innerHTML">
             <input type="text" name="subtask" placeholder="Nouvelle sous-tâche..." required>
