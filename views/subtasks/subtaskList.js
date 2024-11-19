f102b5a4308bbc7c134d37fba66c8d2056dd5285
elisenium <elise.grelaud@student.vinci.be>
2024-11-19 18:08:04 +0100

    Revert "add: display subtask title with task description in subtask list"
    This reverts commit a07bc6a6d51b1b7e4ffe97d7d24240f66e3a8a90.

--- views/subtasks/subtaskList.js
+++ views/subtasks/subtaskList.js
@@ -2,15 +2,11 @@ import createASubtask from './subtask.js';
 
 const createSubtaskList = (subtasks, task) => /*html*/ `
     <div id="subtask-list">
-        <div>
-            <h2>Sous-tâches de la tâche : ${task.description}</h2>
-        </div>
         <form hx-post="/tasks/subtasks/add/${task.id}" hx-target="#subtask-list" hx-swap="innerHTML">
             <input type="text" name="subtask" placeholder="Nouvelle sous-tâche..." required>
             <button type="submit">Ajouter</button>
         </form>
 
-    <div id="subtask-title">
         <table class="todolist">
             ${subtasks.map(subtask => createASubtask(subtask, task)).join('')}
         </table>
