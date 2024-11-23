850116396d402f7ef49651709486af303325d6e5
elisenium <elise.grelaud@student.vinci.be>
2024-11-23 20:59:35 +0100

    create: deployment workflow

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: "dundeal",
+            script: "./app.js",
+            instances: "max",
+            exec_mode: "cluster",
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
+};
